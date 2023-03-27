52fe617087369eb25f6d3ee015cf26fafd79013e
t-kulikova <tatsiana.kulikova@yandex.ru>
2023-03-28 00:01:35 +0300

    Implemented showing the first question

---
+++ MovieQuiz/Presentation/QuizStepViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  QuizStepViewModel.swift
+//  MovieQuiz
+//
+//  Created by Tatsiana Kulikova on 27.03.2023.
+//
+
+import UIKit
+
+// вью модель для состояния "Вопрос показан"
+struct QuizStepViewModel {
+    let image: UIImage
+    let question: String
+    let questionNumber: String
+}
